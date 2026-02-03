ffc957c39edae798e9f6de95f195d600e7846af2
luis angel <20luis1angel7@gmail.com>
2026-02-03 15:09:12 -0400

    cambio local diferente

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ console.log("Hola mundo CAMBIADO desde master");
 console.log("Hola mundo MODIFICADO desde conflicto-test");
 console.log("Estoy aprendiendo Git");
 console.log("Tercer cambio");
-console.log("Cambio desde nueva-funcion");
+console.log("Cambio diferente desde local");
+
