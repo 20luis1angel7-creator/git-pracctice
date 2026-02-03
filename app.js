cbb5b21a6443fcbf9698b6af40eeef08ade20e06
20luis1angel7-creator <20luis1angel7@gmail.com>
2026-02-03 15:01:06 -0400

    Add log statement for GitHub change

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("Hola mundo MODIFICADO desde conflicto-test");
 console.log("Estoy aprendiendo Git");
 console.log("Tercer cambio");
 console.log("Cambio desde nueva-funcion");
+console.log("Cambio hecho desde GitHub");
+
