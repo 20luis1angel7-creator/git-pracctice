8e06fd81b46e0427d81aad4ba75638ca218b667e
luis angel <20luis1angel7@gmail.com>
2026-02-06 15:51:52 -0400

    Add log statement for GitHub change

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ console.log("Hola mundo MODIFICADO desde conflicto-test");
 console.log("Estoy aprendiendo Git");
 console.log("Tercer cambio");
 console.log("Cambio diferente desde local");
+console.log("Cambio desde nueva-funcion");
+console.log("Cambio hecho desde GitHub");
 
