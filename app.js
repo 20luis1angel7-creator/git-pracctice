562867b1a7d16c2b6af7890ac1f6de1abee74214
luis angel <20luis1angel7@gmail.com>
2026-02-06 15:13:52 -0400

    feat: actualizo logs y agrego mensaje final

--- app.js
+++ app.js
@@ -5,4 +5,9 @@ console.log("Tercer cambio");
 console.log("Cambio diferente desde local");
 console.log("Cambio desde nueva-funcion");
 console.log("Cambio hecho desde GitHub");
+console.log("klk");
+
+
+
+
 
